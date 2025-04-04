46d0192f30ee16b258e0c15c21db65d3ca608cb3
sontouf <sontouf@gmail.com>
2025-04-04 22:53:42 +0900

    dD

--- a.c
+++ a.c
@@ -4,5 +4,6 @@ int main() {
     // This is a comment added in Team A's branch
     printf("This is a new line added in Team A's branch.\n");
     perror("Error message from Team A's branch");
+    // This is another comment added in Team A's branch
     return 0;
 }
