9b46ac442fc6b8f6b00022b1666f703fa8033dc9
sontouf <sontouf@gmail.com>
2025-04-04 23:48:25 +0900

    dD

--- a.c
+++ a.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main() {
     printf("Hello from a.c\n");
-    // // This is a comment added in Team A's branch
-    // printf("This is a new line added in Team A's branch.\n");
-    // // perror("Error message from Team A's branch");
-    // // This is another comment added in Team A's branch
+    // This is a comment added in Team A's branch
+    printf("This is a new line added in Team A's branch.\n");
+    // perror("Error message from Team A's branch");
+    // This is another comment added in Team A's branch
     return 0;
 }
