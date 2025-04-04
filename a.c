50bb15d966d52c21bd5e9fd3562a2dd66f0809dd
sontouf <sontouf@gmail.com>
2025-04-04 22:29:03 +0900

    dd

--- a.c
+++ a.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 int main() {
     printf("Hello from a.c\n");
+    // This is a comment added in Team A's branch
+    printf("This is a new line added in Team A's branch.\n");
     return 0;
 }
